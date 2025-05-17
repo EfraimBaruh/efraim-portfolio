7637a32eda8b685ed6b2165dd004f4c65de95c20
EfraimBaruh <efraimbaruhbolukbasi@gmail.com>
2025-05-17 21:18:03 +0300

    Edit navbar to use dropdown for about

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 function Navbar() {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const dropdownRef = useRef(null);
+
+  // Close dropdown when clicking outside
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setIsDropdownOpen(false);
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => document.removeEventListener('mousedown', handleClickOutside);
+  }, []);
+
   return (
     <nav className="navbar">
       <div className="nav-content">
@@ -10,8 +25,25 @@ function Navbar() {
         <ul className="nav-links">
           <li><Link to="/">Home</Link></li>
           <li><Link to="/projects">Projects</Link></li>
-          <li><Link to="/about">Professional</Link></li>
-          <li><Link to="/social">Social</Link></li>
+          <li 
+            className="dropdown"
+            ref={dropdownRef}
+            onMouseEnter={() => setIsDropdownOpen(true)}
+            onMouseLeave={() => setIsDropdownOpen(false)}
+          >
+            <button 
+              className="dropdown-button"
+              onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+            >
+              About
+            </button>
+            {isDropdownOpen && (
+              <ul className="dropdown-menu">
+                <li><Link to="/about" onClick={() => setIsDropdownOpen(false)}>Professional</Link></li>
+                <li><Link to="/social" onClick={() => setIsDropdownOpen(false)}>Social</Link></li>
+              </ul>
+            )}
+          </li>
           <li><Link to="/blog">Blog</Link></li>
           <li><Link to="/contact">Contact</Link></li>
         </ul>
